8db86d2f778f4ed4aea993fbda25156cd06847fa
Akynbek Aizhigitov <akyn@Akynbeks-MacBook-Air.local>
2020-09-12 16:45:46 -0500

    updated

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -43,36 +43,32 @@ def slavePodTemplate = """
             hostPath:
               path: /var/run/docker.sock
     """
-    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
-      node(k8slabel) {
-        stage("Pull the SCM") {
-            git 'https://github.com/akynbek/jenkins-class'
-        }
-        stage("Apply/Plan") {
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+        node(k8slabel) {
             container("fuchicorptools") {
-                if (!params.destroyChanges) {
-                    if (params.applyChanges) {
-                        println("Applying the changes!")
-                    } else {
-                        println("Planing the changes")
-                     }
+                stage("Pull the SCM") {
+                    git 'https://github.com/fsadykov/jenkins-class'
                 }
-                sh 'kubectl version'
+                dir('deployments/k8s') {
+                    stage("Apply/Plan") {
+                        if (!params.destroyChanges) {
+                            if (params.applyChanges) {
+                                println("Applying the changes!")
+                            } else {
+                                println("Planing the changes")
+                            }
+                        }
+                    }
+                    stage("Destroy") {
+                        if (!params.applyChanges) {
+                            if (params.destroyChanges) {
+                                println("Destroying everything")
+                            } 
+                        } else {
+                            println("Sorry I can not destroy and apply!!")
+                        }
+                    }
+                }
             }
-        }
-
-        stage("destroy") {
-            if (!params.applyChanges) {
-                if (params.destroyChanges) {
-                    println("Destroying everything")
-                } 
-            } else {
-                println("Sorry I cannot destroy and apply!!")
-            }
-  
-        }
-
-
-
       }
     }
